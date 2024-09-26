6c8e09e7bcb4b1d262132a132e004864c4ddd7f6
nagipragalathan <nagipragalathan@gmail.com>
2024-09-26 12:52:58 +0530

    "Removed migrate command from build_files.sh and added pytz to requirements.txt"

--- build_files.sh
+++ build_files.sh
@@ -3,5 +3,4 @@ chmod +x build_files.sh
 echo "starting install..."
 python3.12 -m pip install -r requirements.txt
 python3.12 manage.py collectstatic --noinput
-python3.12 manage.py migrate
 echo "end install-----------------------"
